b584cca9af2b4cb88c6e8fcfe841206860b178cd
Brian Street <brian.street2@twcable.com>
2012-11-23 20:30:58 -0700

    TDD: test for no calls
    * removed dir placeholder .gitignores
    * added initial initial interface "FieldCounter"
    * added test for zero records
    * added Spock dependency

---
+++ src/main/groovy/com/github/itgumby/FieldCounter.groovy
@@ -0,0 +1,6 @@
+package com.github.itgumby
+
+public interface FieldCounter {
+    long getRecordCount()
+    long getBlankCount()
+}
---
+++ src/main/groovy/com/github/itgumby/FieldCounterImpl.java
@@ -0,0 +1,13 @@
+package com.github.itgumby;
+
+public class FieldCounterImpl implements FieldCounter {
+  @java.lang.Override
+  public long getRecordCount() {
+    return 0;
+  }
+
+  @java.lang.Override
+  public long getBlankCount() {
+    return 0;
+  }
+}
